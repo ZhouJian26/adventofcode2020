b40cfc68878fe734e0944ce139272777cc674c18
Zhou Jian <zhoujian0998@gmail.com>
2020-12-03 10:04:30 +0100

    Add main.cpp template

---
+++ template/main.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <bitset>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <chrono>
+#include <ctime>
+#include <set>
+using namespace std;
+
+int main() {}
